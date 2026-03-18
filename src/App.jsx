9d6a0b0eff6e3f8ec109d6f021ac423742397588
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2026-03-18 10:32:08 +0530

    removed cursor follower for a clean professional experience

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,6 @@ import TimeComplexityPrimer from "./pages/TimeComplexityPrimer";
 import Links from "./pages/Links";
 import Custom404 from "./pages/404";
 import TerminalWidget from "./components/TerminalWidget";
-import CursorFollower from "./components/CursorFollower";
 
 export default function App() {
   // Preload critical images on app mount
@@ -59,7 +58,6 @@ export default function App() {
 
   return (
     <>
-      <CursorFollower />
       {!isLinksPage && <Navbar />}
       <ScrollToTop />
       <Routes>
